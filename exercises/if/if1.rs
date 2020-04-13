852f9e55e26d574af7bcb8b99712af1bbcb7eb20
Ahmed El Yaacoub <aaey@kth.se>
2020-04-13 19:29:55 +0200

    finished 7 execises

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,8 +1,16 @@
 // if1.rs
 
-// I AM NOT DONE
-
 pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {return a} else {return b}
+/*     if a > b {
+        return a;
+    }
+    else if b > a {
+        return b;
+    }
+    else {
+        return a;
+    } */
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
